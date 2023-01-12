4e1fe2df35372784cbc2ca077633d06011330a92
Jason Howmans <jason@nosaj.io>
2023-01-12 15:55:35 +0000

    camelCase dashes

--- nosaj-io/next.config.js
+++ nosaj-io/next.config.js
@@ -1,6 +1,22 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  transpilePackages: ["data"],
+  webpack: (config) => {
+    // camelCase style names from css modules
+    config.module.rules
+      .find(({ oneOf }) => !!oneOf)
+      .oneOf.filter(({ use }) => JSON.stringify(use)?.includes("css-loader"))
+      .reduce((acc, { use }) => acc.concat(use), [])
+      .forEach(({ options }) => {
+        if (options.modules) {
+          // 'dashes' will only camelCase "-" words, ie. my_class-name => my_className
+          options.modules.exportLocalsConvention = "dashes";
+        }
+      });
 
-module.exports = nextConfig
+    return config;
+  },
+};
+
+module.exports = nextConfig;
