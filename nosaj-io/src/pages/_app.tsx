7b66e7146436652e1a035f40b37d5f21da546392
Jason Howmans <jason@nosaj.io>
2023-01-29 14:46:51 +0000

    Add Google Analytics

--- nosaj-io/src/pages/_app.tsx
+++ nosaj-io/src/pages/_app.tsx
@@ -1,6 +1,23 @@
 import 'styles/globals.scss';
 import type { AppProps } from 'next/app';
+import Script from 'next/script';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Script
+        src="https://www.googletagmanager.com/gtag/js?id=G-7V8JX5MX4D"
+        strategy="afterInteractive"
+      />
+      <Script id="ga" strategy="afterInteractive">
+        {`
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+        gtag('config', 'G-7V8JX5MX4D');
+        `}
+      </Script>
+      <Component {...pageProps} />
+    </>
+  );
 }
