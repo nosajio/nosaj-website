49374e4afcfc9fe4aa07004cb5555ac0c68c605f
Jason Howmans <jason@nosaj.io>
2023-01-30 18:26:31 +0000

    Swallow exceptions from `newEvent` calls

--- data/src/queries/events.ts
+++ data/src/queries/events.ts
@@ -5,9 +5,16 @@ export const newEvent = async <T extends AnyEvent['event']>(
   type: T,
   metadata?: Extract<AnyEvent, { event: T }>['metadata'],
 ) => {
-  const [createdEvent] = await query<AnyEvent>(
-    'insert into events (event, metadata) values($1, $2::jsonb) returning *',
-    [type, metadata ?? {}],
-  );
-  return createdEvent;
+  try {
+    const [createdEvent] = await query<AnyEvent>(
+      'insert into events (event, metadata) values($1, $2::jsonb) returning *',
+      [type, metadata ?? {}],
+    );
+    return createdEvent;
+  } catch (err) {
+    // Swallow errors when writing events. No exceptions should occur based on
+    // this operation since it's a side-effect of other operations.
+    console.error(err);
+    return undefined;
+  }
 };
