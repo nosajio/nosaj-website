f662ac0ffffafc092ed4cc7dcc6b918fa936a56c
Jason Howmans <jason@nosaj.io>
2023-01-31 13:28:26 +0000

    Add subject to email, remove SES from project

--- nosaj-io/src/utils/emails.ts
+++ nosaj-io/src/utils/emails.ts
@@ -1,21 +1,18 @@
-import { SendEmailCommand, SESClient } from '@aws-sdk/client-ses';
-import { getSubscriberByEmail } from 'data/server';
+import {
+  getSubscriberByEmail,
+  getUnsubscribeLink,
+  rootNosajURL,
+  sendEmail,
+} from 'data/server';
 
-const ses = new SESClient({ region: 'us-east-1' });
-
-const rootNosajURL =
-  process.env.NODE_ENV === 'development'
-    ? `http://localhost:3003`
-    : 'https://nosaj.io';
-
+/**
+ * Define globals and helpers for email operations
+ */
 const getConfirmLink = (token: string) => `${rootNosajURL}?token=${token}`;
 
-const getUnsubscribeLink = (token: string, emailId?: string) =>
-  `${rootNosajURL}/unsubscribe?token=${token}&eid=${emailId ?? 'na'}`;
-
-const noreplySenderAddress = 'Jason<noreply@email.nosaj.io>';
-const replyToAddress = 'newsletter@nosaj.io';
-
+/**
+ * Send newsletter signup confirmation message
+ */
 export const sendConfirmEmail = async (email: string) => {
   // Get subscriber from db, ensure they're not yet confirmed
   const subscriber = await getSubscriberByEmail(email);
@@ -43,47 +40,10 @@ export const sendConfirmEmail = async (email: string) => {
   <p>PS - changed your mind? <a href="${unsubscribeLink}">Unsubscribe</a></p>
   `;
   const emailPlain = `Please follow this link to confirm your email: ${confirmLink}`;
-
-  // Send email
-  try {
-    const cmd = new SendEmailCommand({
-      ReplyToAddresses: [replyToAddress],
-      Destination: {
-        ToAddresses: [subscriber.email],
-      },
-      Source: noreplySenderAddress,
-      Message: {
-        Body: {
-          Html: {
-            Data: emailHTML,
-          },
-          Text: {
-            Data: emailPlain,
-          },
-        },
-        Subject: {
-          Data: 'Please confirm your email',
-        },
-      },
-    });
-    const receipt = await ses.send(cmd);
-    if (!receipt.MessageId) {
-      console.error(
-        'Failed to send confirm email to: %s. Details: %o',
-        subscriber.email,
-        receipt,
-      );
-      return false;
-    } else {
-      console.log(
-        'Sent email to %s, MessageId: %s',
-        subscriber.email,
-        receipt.MessageId,
-      );
-      return true;
-    }
-  } catch (err) {
-    console.error(err);
-    return false;
-  }
+  return await sendEmail(
+    subscriber.email,
+    'Please confirm your email',
+    emailHTML,
+    emailPlain,
+  );
 };
